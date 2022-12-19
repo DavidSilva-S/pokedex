6feac53f2268a23cb39347600f2303db73654ba1
addy-dclxvi <addy4rock@gmail.com>
2022-12-19 16:30:03 -0300

    feature: "add animation side bar"

--- scripts/handleButtonsEvents.js
+++ scripts/handleButtonsEvents.js
@@ -7,25 +7,23 @@ const inputUser = document.querySelector('.search-pokemon')
 const buttonSearchPokemon = document.querySelector('.btn-search')
 const menuHamburguer = document.querySelector('.pokedex-footer-bars-container')
 const sidePokedex = document.querySelector('.side-pokedex')
-let idPokemon = 1;
+let idPokemon = 0;
 let toggle = false
 
 menuHamburguer.addEventListener('click', () => {
-  console.log(toggle);
   toggle = !toggle
-  sidePokedex.style.transform =  toggle ? "translateX(1500px)" : "translateX(0px)"
-  sidePokedex.style.transition = "all 1s ease-in-out"
-  // sidePokedex.style.opacity = toggle? "1" : "0"
+  sidePokedex.style.transform = toggle ? "translateX(-400px)" : ""
+  sidePokedex.style.transition = "all ease-out 1s"
 })
 
-window.addEventListener("keyup", async(e) => {
-  if (e.key !== 'Enter') return; 
+window.addEventListener("keyup", async (e) => {
+  if (e.key !== 'Enter') return;
   searchPokemonByItsName(inputUser.value)
 })
 
 buttonSearchPokemon.addEventListener("click", () => {
-  console.log(buttonSearchPokemon);
   searchPokemonByItsName(inputUser.value)
+  inputUser.value = ""
 })
 
 window.addEventListener("keyup", async ({ key }) => {
@@ -45,12 +43,12 @@ arrowLeft.addEventListener("click", async () => {
   searchArrowLeft()
 });
 
-arrowRight.addEventListener("click", async() => {
+arrowRight.addEventListener("click", async () => {
   searchArrowRight()
 });
 
 async function searchPokemonByItsName(inputUser) {
-  const { id, name, sprites, stats } = await getPokemonByItsName(inputUser)
+  const { id, name, sprites, stats, weight, height } = await getPokemonByItsName(inputUser)
   const infoPokemon = {
     id,
     name,
@@ -60,45 +58,51 @@ async function searchPokemonByItsName(inputUser) {
       def: stats[2].base_stat,
       hp: stats[0].base_stat,
       speed: stats[5].base_stat,
+      weight,
+      height
     }
   }
 
-  handleUpdateDisplay(infoPokemon.sprite, infoPokemon.name, infoPokemon.status) 
+  handleUpdateDisplay(infoPokemon.sprite, infoPokemon.name, infoPokemon)
 }
 
 async function searchArrowLeft() {
   if (idPokemon === 1) return;
   idPokemon--;
 
-  const { id, name, sprites, stats } = await getPokemonById(idPokemon)
-      const infoPokemon = {
-        id,
-        name,
-        sprite: sprites.versions["generation-v"]["black-white"].animated.front_default,
-        status: {
-          atk: stats[1].base_stat,
-          def: stats[2].base_stat,
-          hp: stats[0].base_stat,
-          speed: stats[5].base_stat,
-        }
-      }
-      handleUpdateDisplay(infoPokemon.sprite, infoPokemon.name, infoPokemon.status)  
+  const { id, name, sprites, stats, weight, height  } = await getPokemonById(idPokemon)
+  const infoPokemon = {
+    id,
+    name,
+    sprite: sprites.versions["generation-v"]["black-white"].animated.front_default,
+    status: {
+      atk: stats[1].base_stat,
+      def: stats[2].base_stat,
+      hp: stats[0].base_stat,
+      speed: stats[5].base_stat,
+      weight, 
+      height 
+    }
+  }
+  handleUpdateDisplay(infoPokemon.sprite, infoPokemon.name, infoPokemon)
 }
 
 async function searchArrowRight() {
   if (idPokemon >= 905) return alert("The limit of pokemon reached!");
-  idPokemon ++;
-  const { id, name, sprites, stats } = await getPokemonById(idPokemon)
-      const infoPokemon = {
-        id,
-        name,
-        sprite: sprites.versions["generation-v"]["black-white"].animated.front_default,
-        status: {
-          atk: stats[1].base_stat,
-          def: stats[2].base_stat,
-          hp: stats[0].base_stat,
-          speed: stats[5].base_stat,
-        }
-      }
-      handleUpdateDisplay(infoPokemon.sprite, infoPokemon.name, infoPokemon.status)
+  idPokemon++;
+  const { id, name, sprites, stats, weight, height  } = await getPokemonById(idPokemon)
+  const infoPokemon = {
+    id,
+    name,
+    sprite: sprites.versions["generation-v"]["black-white"].animated.front_default,
+    status: {
+      atk: stats[1].base_stat,
+      def: stats[2].base_stat,
+      hp: stats[0].base_stat,
+      speed: stats[5].base_stat,
+      weight, 
+      height 
+    }
+  }
+  handleUpdateDisplay(infoPokemon.sprite, infoPokemon.name, infoPokemon)
 }
--- scripts/handleUpdateDisplay.js
+++ scripts/handleUpdateDisplay.js
@@ -3,15 +3,20 @@ const pokemonScreenName = document.querySelector('.screen-name-pokemon')
 const pokemonSideScrenImg = document.querySelector('.side-pokedex-screen img')
 const pokemonPokemonStatus = document.querySelector('.side-pokedex-screen .side-screen-status')
 
-export default function handleUpdateDisplay(spriteImage, name, status) {
+export default function handleUpdateDisplay(spriteImage, name, {id, status}) {
     pokemonScreenIn.firstElementChild.src = spriteImage
-    pokemonScreenName.textContent = name.charAt(0).toUpperCase() + name.slice(1)
+    pokemonScreenName.innerHTML = `
+    <h2>${name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+    <span> weight: ${Math.floor(status.weight / 2.2046)} kg </span>
+    <span> height: ${Math.floor(status.height / 3.2808)} m </span>    
+    `
     pokemonSideScrenImg.src = `http://play.pokemonshowdown.com/sprites/ani/${name}.gif`
     pokemonPokemonStatus.innerHTML = `
-    <p>atk: ${status.atk}<p>
-    <p>def: ${status.def}<p>
-    <p>hp: ${status.hp}<p>
-    <p>speed: ${status.speed}<p>
+        <span style="position: absolute;left: 3%;bottom: 3%;">#${id} </span>
+        <p>atk: ${status.atk}<p>
+        <p>def: ${status.def}<p>
+        <p>hp: ${status.hp}<p>
+        <p>speed: ${status.speed}<p>
 `
 }
 
